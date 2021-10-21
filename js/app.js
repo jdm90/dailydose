1f76b0d0aa724800f91ed8b6dbe12c1cd21f04e3
Justin Miles <jdmiles90@gmail.com>
2021-10-20 23:57:45 -0500

    more big changes

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function animateToTop(e) {
+    e.preventDefault();
+    var scrollToTop = window.setInterval(function() {
+        var pos = window.pageYOffset;
+        if ( pos > 0 ) {
+            window.scrollTo( 0, pos - 20 );
+        } else {
+            window.clearInterval( scrollToTop );
+        }
+    }, 3);
+}
